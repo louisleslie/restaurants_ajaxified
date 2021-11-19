34d4cc98dea41e2dcc37d78bf0cc5aaeb51573cc
Louis Leslie <louis.leslie@gmail.com>
2021-11-19 06:59:16 +0000

    Add review to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
   Rails.application.routes.draw do
-    resources :restaurants, only: [ :index, :show ]
+    resources :restaurants, only: [ :index, :show ] do
+      resources :reviews, only: :create
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
