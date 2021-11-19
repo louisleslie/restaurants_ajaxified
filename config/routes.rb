31ddf85a33d20107c75deed3217d8da9767b07b2
Louis Leslie <louis.leslie@gmail.com>
2021-11-19 10:17:30 +0000

    Added AJAX

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
   Rails.application.routes.draw do
-    resources :restaurants, only: [ :index, :show ] do
+    resources :restaurants, only: [ :index, :show , :destroy] do
       resources :reviews, only: :create
     end
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
